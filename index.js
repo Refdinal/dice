c9e8da48e5383f45a1e401fa4f12f2e46e07a2f9
Refdinal <136530969+Refdinal@users.noreply.github.com>
2023-08-04 19:30:25 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1 * 6 + 1);
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2 * 6 + 1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomimages1 = "./images/dice" + randomNumber1 + ".png";
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomimages2 = "./images/dice" + randomNumber2 + ".png";
 
-if (randomNumber1 === 1) {
-  document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
-} else if (randomNumber1 === 2) {
-  document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
-} else if (randomNumber1 === 3) {
-  document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
-} else if (randomNumber1 === 4) {
-  document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
-} else if (randomNumber1 === 5) {
-  document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
-} else {
-  document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
-}
-
-if (randomNumber2 === 1) {
-  document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
-} else if (randomNumber2 === 2) {
-  document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
-} else if (randomNumber2 === 3) {
-  document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
-} else if (randomNumber2 === 4) {
-  document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-} else if (randomNumber2 === 5) {
-  document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
-} else {
-  document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
-}
+document.querySelectorAll("img")[0].setAttribute("src", randomimages1);
+document.querySelectorAll("img")[1].setAttribute("src", randomimages2);
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Win";
